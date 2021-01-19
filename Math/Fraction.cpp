a704e254c0b2f95b708ec723c61194c379e59912
George056 <70665330+George056@users.noreply.github.com>
2021-01-19 09:15:38 -0600

    Fraction updated

--- Math/Fraction.cpp
+++ Math/Fraction.cpp
@@ -1,45 +1,112 @@
 #include "Fraction.h"
 
 namespace blib{
-  fraction fraction::operator +(fraction right){
-    return fraction(__numerator + right.__numerator, __denominator + right.__denominator);
+  fraction fraction::operator +(fraction right) const {
+    long long num_l = __numerator, num_2 = right.__numerator;
+    long long denum = lcm(__denominator, right.__denominator);
+    num1 *= denum/__denominator;
+    num2 *= denum/right.__denominator;
+    return fraction(num1 + num2, denum);
   }
-  fraction fraction::operator +(long long right){
-    return fraction(__numerator + right, __denominator);
-  }
-
-  fraction fraction::operator -(fraction right){
-    return fraction(__numerator - right.__numerator, __denominator - right.__denominator);
-  }
-  fraction fraction::operator -(long long right){
-    return fraction(__numerator - right, __denominator);
+  fraction fraction::operator +(long long right) const {
+    return fraction(__numerator + (right * __denominator), __denominator);
   }
 
-  fraction fraction::operator -(){
+  fraction fraction::operator -(fraction right) const {
+    long long num_l = __numerator, num_2 = right.__numerator;
+    long long denum = lcm(__denominator, right.__denominator);
+    num1 *= denum/__denominator;
+    num2 *= denum/right.__denominator;
+    return fraction(num1 - num2, denum);
+  }
+  fraction fraction::operator -(long long right) const {
+    return fraction(__numerator - (right * __denominator), __denominator);
+  }
+
+  fraction fraction::operator -() const {
     return fraction(-__numerator, __denominator);
   }
 
-  fraction fraction::operator *(fraction right){
+  fraction fraction::operator *(fraction right) const {
     return fraction(__numerator * right.__numerator, __denominator * right.__denominator);
   }
-  fraction fraction::operator *(long long right){
+  fraction fraction::operator *(long long right) const {
     return fraction(__numerator * right, __denominator);
   }
 
-  fraction fraction::operator /(fraction right);
-  fraction fraction::operator /(long long right);
+  fraction fraction::operator /(fraction right) const {
+    return fraction(__numerator * right.__denominator, __denominator * right.__numerator);
+  }
+  fraction fraction::operator /(long long right) const {
+    return fraction(__numerator, __denominator * right);
+  }
 
-  fraction& fraction::operator +=(fraction right);
-  fraction& fraction::operator +=(long long right);
+  fraction& fraction::operator +=(fraction right){
+    fraction temp = *this + right;
+    __numerator = temp.__numerator;
+    __denominator = temp.__denominator;
+    return *this;
+  }
+  fraction& fraction::operator +=(long long right){
+    __numerator += (right * __denominator);
+    return *this;
+  }
 
-  fraction& fraction::operator -=(fraction right);
-  fraction& fraction::operator -=(long long right);
+  fraction& fraction::operator -=(fraction right){
+    fraction temp = *this - right;
+    __numerator = temp.__numerator;
+    __denominator = temp.__denominator;
+    return *this;
+  }
+  fraction& fraction::operator -=(long long right){
+    __numerator -= (right * __denominator);
+    return *this;
+  }
 
-  fraction& fraction::operator *=(fraction right);
-  fraction& fraction::operator *=(long long right);
+  fraction& fraction::operator *=(fraction right){
+    __numerator *= right.__numerator;
+    __denominator *= right.__denominator;
+    return *this;
+  }
+  fraction& fraction::operator *=(long long right){
+    __numerator *= right;
+    return *this;
+  }
 
-  fraction& fraction::operator /=(fraction right);
-  fraction& fraction::operator /=(long long right);
+  fraction& fraction::operator /=(fraction right){
+    __numerator *= right.__denominator;
+    __denominator *= right.__numerator;
+    return *this;
+  }
+  fraction& fraction::operator /=(long long right){
+    __denominator *= right;
+    return *this;
+  }
 
-  fraction::operator double() const;
+  fraction::operator float() const{
+    return __numerator / __denominator;
+  }
+  fraction::operator double() const{
+    return __numerator / __denominator;
+  }
+  fraction::operator long double() const{
+    return __numerator / __denominator;
+  }
+  
+  fraction fraction::inverse(){
+    return fraction(__denominator, __numerator);
+  }
+
+  fraction operator +(long long left, fraction right){
+    return right + left;
+  }
+  fraction operator -(long long left, fraction right){
+    return (-right) + left;
+  }
+  fraction operator *(long long left, fraction right){
+    return right * left;
+  }
+  fraction operator /(long long left, fraction right){
+    return right.inverse() * left;
+  }
 }
--- Math/Fraction.h
+++ Math/Fraction.h
@@ -1,6 +1,8 @@
 #ifndef FRACTION
 #define FRACTION
 
+#include "Functions.h"
+
 namespace blib{
 
   class fraction{
@@ -23,19 +25,19 @@ namespace blib{
     inline long long numerator(){ return __numerator; }
     inline long long denominator(){ return __denominator; }
 
-    fraction operator +(fraction);
-    fraction operator +(long long);
+    fraction operator +(fraction) const;
+    fraction operator +(long long) const;
 
-    fraction operator -(fraction);
-    fraction operator -(long long);
+    fraction operator -(fraction) const;
+    fraction operator -(long long) const;
 
-    fraction operator -();
+    inline fraction operator -() const;
 
-    fraction operator *(fraction);
-    fraction operator *(long long);
+    inline fraction operator *(fraction) const;
+    inline fraction operator *(long long) const;
 
-    fraction operator /(fraction);
-    fraction operator /(long long);
+    inline fraction operator /(fraction) const;
+    inline fraction operator /(long long) const;
 
     fraction& operator +=(fraction);
     fraction& operator +=(long long);
@@ -43,17 +45,24 @@ namespace blib{
     fraction& operator -=(fraction);
     fraction& operator -=(long long);
 
-    fraction& operator *=(fraction);
-    fraction& operator *=(long long);
+    inline fraction& operator *=(fraction);
+    inline fraction& operator *=(long long);
 
-    fraction& operator /=(fraction);
-    fraction& operator /=(long long);
+    inline fraction& operator /=(fraction);
+    inline fraction& operator /=(long long);
 
+    operator float() const;
     operator double() const;
+    operator long double() const;
 
     fraction inverse();
 
   }
+
+  inline fraction operator +(long long, fraction);
+  inline fraction operator -(long long, fraction);
+  inline fraction operator *(long long, fraction);
+  inline fraction operator /(long long, fraction);
 
 }
 
